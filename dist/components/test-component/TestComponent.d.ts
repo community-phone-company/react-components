ce2af46424c83bedb6e6783d8b4bed720cd0f80b
Igor M <russo.programmisto@gmail.com>
2022-03-31 21:46:18 +0300

    Added dist folder to repository

---
+++ dist/components/test-component/TestComponent.d.ts
@@ -0,0 +1,10 @@
+import React from "react";
+export default class TestComponent extends React.Component<Props, State> {
+    constructor(props: Props);
+    render(): JSX.Element;
+}
+interface Props {
+}
+interface State {
+}
+export {};
---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+import TestComponent from "./components/test-component/TestComponent";
+export { TestComponent };
